df3c95147f87af87113f02559a016498e9ff8141
amac2d <amac2d@student.mtsac.edu>
2019-07-12 06:02:11 +0000

    Add a componentDidMount method to ProductDetails that retrieves the details of a specific product by id from the server with a GET request. e.g. '/api/products.php?id=1' Complete the implementation of ProductDetails's render method to conditionally render a view of the product's details (as soon as the details are loaded from the server). Add a property to App's initial state named view that is an Object with two properties; name and params. name is a String set to catalog and params is an empty Object. i.e. { view: 'catalog', params: {} } Define a setView method in App that takes two parameters; name and params. setView updates App's view state to the new name String and params Object. Pass App's setView method into ProductList via props so that when a user clicks on a ProductListItem, App's view state gets updated to { name: 'details', params: { id: product.id } } Modify App's render method to conditionally render either the ProductList or the ProductDetails based on whether App's current view.name state is catalog or details. If the ProductDetails component is being rendered, pass it App's view.params state via props as well as App's setView method.  Modify ProductDetails's componentDidMount method to request product details based on the props it now receives from App. Ensure that when the user clicks 'Back to catalog' in ProductDetails, App's setView callback gets called with 'catalog' and an empty Object to switch the user back to the ProductList view.

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import Header from './header';
 import ProductList from './product-list';
+import ProductDetails from './product-details';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      products: []
+      products: [],
+      view: {
+        name: 'catalog',
+        params: {}
+      }
     };
-
+    this.setView = this.setView.bind(this);
   }
   componentDidMount() {
     this.getProducts();
@@ -20,12 +25,29 @@ export default class App extends React.Component {
         this.setState({ products: successObj });
       });
   }
+  setView(name, params) {
+    this.setState({
+      view: {
+        name,
+        params
+      }
+    });
+  }
   render() {
-    return (
-      <div>
-        <Header text='Wicked Sales' />
-        <ProductList products={this.state.products}/>
-      </div>
-    );
+    if (this.state.view.name === 'catalog') {
+      return (
+        <div>
+          <Header text='Wicked Sales' />
+          <ProductList click={this.setView} products={this.state.products} />
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <Header text='Wicked Sales' />
+          <ProductDetails params={this.state.view.params} click={this.setView}/>
+        </div>
+      );
+    }
   }
 }
--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 
 class ProductListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.sendViewStateObj = this.sendViewStateObj.bind(this);
+  }
   componentDidMount() {
-    console.log('this.props.element:', this.props.element);
+    // console.log('this.props.element:', this.props.element);
+  }
+  sendViewStateObj() {
+    const viewStateObj = {
+      name: 'details',
+      params: {
+        id: this.props.element.id
+      }
+    };
+    this.props.click(viewStateObj.name, viewStateObj.params);
   }
   render() {
     const img = this.props.element.image;
     const imgStyle = {
       backgroundImage: 'url(' + img + ')',
       height: '300px',
-      widtch: '300px',
       backgroundSize: '100% 100%',
       backgroundRepeat: 'no-repeat',
       objectFit: 'contain'
@@ -19,7 +31,7 @@ class ProductListItem extends React.Component {
         <div className='productCard card mb-4 shadow-sm'>
           <div className='productImage card-img-top' style={imgStyle}></div>
           <div className='productCardBody card-body'>
-            <p className='productName card-title'>
+            <p className='productName card-title' onClick={this.sendViewStateObj}>
               <strong>{this.props.element.name}</strong>
             </p>
             <p className='productPrice card-text'>
