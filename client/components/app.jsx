8ffad9c38cb09bf420f51a028d20f298d54ad18c
amac2d <amac2d@student.mtsac.edu>
2019-07-12 20:06:00 +0000

    Add an initial state property to App named cart set to an empty Array. Define a method in App named getCartItems that retrieves the user's cart items with a GET request to ‘/api/cart.php’ Load all of the user's cart items from the server by calling getCartItems in App's componentDidMount method. Modify Header to receive a cartItemCount prop that is the number of items in App's cart state. Header should display a shopping cart icon with the number of items in the user's cart.  Define a method in App named addToCart with one parameter, product, which is a product Object. addToCart should POST the product to ‘/api/cart.php’ before adding it to the App's cart state.  Modify ProductDetails to include an ‘Add to Cart’ button.
     In App's render method, pass App's addToCart method to ProductDetails via props so that when a user clicks the ‘Add to Cart’ button, the product is added to the cart.
    
    still have console logs commented out for now
    also fixed the header to show item count for when the page is in product details page
    also left console log in component did mount in product list item component
    fixed the back to catalog button to a clickable div text to look like the figma

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -11,12 +11,15 @@ export default class App extends React.Component {
       view: {
         name: 'catalog',
         params: {}
-      }
+      },
+      cart: []
     };
     this.setView = this.setView.bind(this);
+    this.addToCart = this.addToCart.bind(this);
   }
   componentDidMount() {
     this.getProducts();
+    this.getCartItems();
   }
   getProducts() {
     fetch('/api/products.php')
@@ -33,19 +36,40 @@ export default class App extends React.Component {
       }
     });
   }
+  getCartItems() {
+    fetch('/api/cart.php')
+      .then(promiseObj => promiseObj.json())
+      .then(successObj => {
+        this.setState({ cart: successObj });
+      });
+  }
+  addToCart(product) {
+    fetch('/api/cart.php', {
+      method: 'POST',
+      body: JSON.stringify(product),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(promiseObj => promiseObj.json())
+      .then(successObj => {
+        this.setState({ cart: [...this.state.cart, successObj] });
+      })
+      .catch(error => console.error('Error:', error));
+  }
   render() {
     if (this.state.view.name === 'catalog') {
       return (
         <div>
-          <Header text='Wicked Sales' />
+          <Header text='Wicked Sales' cartItemCount={this.state.cart.length} />
           <ProductList click={this.setView} products={this.state.products} />
         </div>
       );
     } else {
       return (
         <div>
-          <Header text='Wicked Sales' />
-          <ProductDetails params={this.state.view.params} click={this.setView}/>
+          <Header text='Wicked Sales' cartItemCount={this.state.cart.length} />
+          <ProductDetails params={this.state.view.params} click={this.setView} addToCart={this.addToCart} />
         </div>
       );
     }
