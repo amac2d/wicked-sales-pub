5259bf6ece97c91ce5909fd5bafca240622bbd65
amac2d <amac2d@student.mtsac.edu>
2019-07-16 04:17:06 +0000

    added exception

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -3,16 +3,20 @@
 require_once('functions.php');
 require_once('db_connection.php');
 
-set_exception_handler( 'error_handler' );
+set_exception_handler('error_handler');
 startup();
 
-if( !empty( $_GET["id"] ) ) {
-  $whereClause = " WHERE `id` = " . $_GET["id"]; 
+if (!empty($_GET["id"])) {
+  $id = $_GET["id"];
+  if (!is_numeric($id)) {
+    throw new Exception('id needs to be a number');
+  }
+  $whereClause = " WHERE `id` = " . $id;
 } else {
   $whereClause = "";
 }
 
-if(!$conn) {
+if (!$conn) {
   throw new Exception('exception: ' . mysqli_connect_error());
   exit();
 }
@@ -21,21 +25,19 @@ if(!$conn) {
 $query = "SELECT * FROM `products`" . $whereClause;
 
 // print( $output ); //delete if needed
-$result = mysqli_query( $conn, $query );
+$result = mysqli_query($conn, $query);
 
-if(!$result) {
+if (!$result) {
   throw new Exception('exception of result: ' . mysqli_error($conn));
   exit();
 }
 
 $output = [];
 
-while($row = mysqli_fetch_assoc($result)) {
+while ($row = mysqli_fetch_assoc($result)) {
   array_push($output, $row);
 }
 
 $json_output = json_encode($output);
 
 print($json_output);
-
-?>
